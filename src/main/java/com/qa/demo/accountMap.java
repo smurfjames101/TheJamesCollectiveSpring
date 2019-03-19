22d1a0c1bfd2927c0c7e0f309c1cd57d186e3f03
smurfjames101 <smurfjames@gmail.com>
2019-03-19 10:57:44 +0000

    Removed Useless Files

--- src/main/java/com/qa/demo/accountMap.java
+++ src/main/java/com/qa/demo/accountMap.java
@@ -3,19 +3,25 @@ package com.qa.demo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Component
+public class AccountMap {
+	@Autowired
+	private INumGen numGen;
 
-public class accountMap {
+	public Map<String, Account> map = new HashMap<String, Account>();
 
-	//@Scope("prototype")
-	public Map<String, Account> maps = new HashMap<String, Account>();
-	
-	public void addAccount(ApplicationContext ac, int numLength) {
-		maps.put(((AccountNumberGenerator)ac.getBean("accountNumberGenerator")).genAccountNum(numLength),((Account)ac.getBean("account")));
+	public void addAccount(Account account) {
+		map.put(numGen.genNum(), account);
+	}
+	@Override
+	public String toString() {
+		return this.map.toString();
 	}
 }
